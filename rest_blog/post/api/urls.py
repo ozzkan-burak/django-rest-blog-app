e773ec924491be356549b92643cd0fdf0a792c29
Burak Özkan <burak.ozkan@path.com.tr>
2021-12-22 01:02:31 +0300

    add detail query

--- rest_blog/post/api/urls.py
+++ rest_blog/post/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from post.api.views import PostListAPIView
+from post.api.views import PostListAPIView, PostDetailAPIView
 
 urlpatterns = [
   path('list', PostListAPIView.as_view(), name='list'),
+  path('detail/<slug>', PostDetailAPIView.as_view(), name='detail'),
 ]
--- rest_blog/post/api/views.py
+++ rest_blog/post/api/views.py
@@ -1,6 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from post.models import Post
+from post.api.serializers import PostSerializer
 
 
 class PostListAPIView(ListAPIView):
   queryset = Post.objects.all()
+  serializer_class = PostSerializer
+  
+class PostDetailAPIView(RetrieveAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+  lookup_field = 'slug'
