9207c6cebbeabf02b2fbaeb9b9354c6f909d82f5
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-26 17:15:43 +0300

    add farourite module

---
+++ rest_blog/favourite/api/serializers.py
@@ -0,0 +1,17 @@
+from favourite.models import Favourite
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+class FavouriteListCreateAPISerializer(ModelSerializer):
+  class Meta:
+    model =  Favourite
+    fields = '__all__'
+    
+  def validate(self, attrs):
+    
+    queryset = Favourite.objects.filter(user=attrs['user'], post=attrs['post'])
+    
+    if queryset.exists():
+      raise serializers.ValidationError("This is an existing favourite")
+    return attrs    
+    
