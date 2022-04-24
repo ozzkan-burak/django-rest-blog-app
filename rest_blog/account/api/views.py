a8dc8a34ee52f5b7be7078a2dbc1c01cb88106cd
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-04-24 17:56:54 +0300

    episode22

--- rest_blog/account/api/views.py
+++ rest_blog/account/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import RetrieveUpdateAPIView,get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
+from rest_framework.views import APIView 
 from django.shortcuts import render
+from rest_framework.response import Response
 
-from account.api.serializers import UserSerializer
+from account.api.serializers import UserSerializer, ChangePasswordSerializer
 
 class ProfileView(RetrieveUpdateAPIView):
     """
@@ -22,4 +24,31 @@ class ProfileView(RetrieveUpdateAPIView):
     def perfom_update(self, serializer):
       serializer.save(user = self.request.user)
 
+class UpdatePassword(APIView):
+    """
+    Update password
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset):
+      return self.request.user
+    
+    def put(self, request, * args, **kwargs):
+      self.object = self.get_object()
+      data = {
+        'old_password': request.data['old_password'],
+        'new_password': request.data['new_password']
+      }
+      serializer = ChangePasswordSerializer(data = request.data)
+      if serializer.is_valid():
+        # Check old password
+        old_password = serializer.data.get("old_password")
+        if not self.object.check_password(old_password):
+          return Response({"old_password": ["Wrong password."]}, status = 400)
+        # set_password also hashes the password that the user will get
+        self.object.set_password(serializer.data.get("new_password"))
+        self.object.save()
+        return Response(status = 200)
+      return Response(serializer.errors, status = 400)
     
